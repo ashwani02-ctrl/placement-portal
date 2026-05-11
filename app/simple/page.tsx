99a6a2af17c67cd87d30c3c6e83c2edb9725e183
Ashwani <ashwanishar15@gmail.com>
2026-05-11 23:33:10 +0530

    resume-generator done

--- app/simple/page.tsx
+++ app/simple/page.tsx
@@ -1,5 +1,9 @@
 import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
+import { useState } from "react";
 
-export default function Page() {
-  return <SimpleEditor />
-}
+  export default function Page() {
+    const [content, setContent] = useState("");
+    return <SimpleEditor
+      value={content}
+      onChange={setContent} />
+  }
